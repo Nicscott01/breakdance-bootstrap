67871a544ec61daf440efc855fe1dff84dcf7e67
Nicolas Scott <nic@crearewebsolutions.com>
2024-02-19 16:45:19 -0500

    bump version

--- breakdance-bootstrap.php
+++ breakdance-bootstrap.php
@@ -3,7 +3,7 @@
  *  Plugin Name: Breakdance Bootstrap
  *  Description: Some baseline additions for websites built with Breakdance.
  *  Author: Nic Scott
- *  Version: 0.1
+ *  Version: 0.2
  * 
  * 
  */
