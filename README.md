d1d3606f99a85b820cc16832557d110406335dcb
Nicolas Scott <nic@crearewebsolutions.com>
2025-03-27 15:50:02 -0400

    update ver

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Bootstrap features for Breakdance sites
 
 ## Changelog
-### 3/17/2025 v1.0.5 
+### 3/17/2025 v1.0.5 /6
 - Require Breakdance in wp plugin header
 - Add "Press List" element
 ### 3/14/2025 v1.0.4
--- breakdance-bootstrap.php
+++ breakdance-bootstrap.php
@@ -3,12 +3,12 @@
  *  Plugin Name: Breakdance Bootstrap
  *  Description: Some baseline additions for websites built with Breakdance.
  *  Author: Nic Scott
- *  Version: 1.0.5
+ *  Version: 1.0.6
  *  Requires Plugins: breakdance
  * 
  */
 
- define( 'BREAKDANCE_BS_VERSION', '1.0.5' );
+ define( 'BREAKDANCE_BS_VERSION', '1.0.6' );
 
  use BricBreakdance\GatedDownloadFormHandler;
  use BricBreakdance\FluentCrmFormHandler;
