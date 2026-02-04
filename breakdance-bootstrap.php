0bc0ebe16824320e44e3faa972de2381a3b1f4b8
Nicolas Scott <nic@crearewebsolutions.com>
2026-02-04 10:26:40 -0500

    add Circle Counter w/ dynamic data

--- breakdance-bootstrap.php
+++ breakdance-bootstrap.php
@@ -3,12 +3,12 @@
  *  Plugin Name: Breakdance Bootstrap
  *  Description: Some baseline additions for websites built with Breakdance.
  *  Author: Nic Scott
- *  Version: 1.2.10
+ *  Version: 1.2.11
  *  Requires Plugins: breakdance
  * 
  */
 
- define( 'BREAKDANCE_BS_VERSION', '1.2.10' );
+ define( 'BREAKDANCE_BS_VERSION', '1.2.11' );
 
  use BricBreakdance\GatedDownloadFormHandler;
  use BricBreakdance\FluentCrmFormHandler;
